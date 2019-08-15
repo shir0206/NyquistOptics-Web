13d72a5a4f0b098ec664e624ffc32c103376b929
shir0206 <40990488+shir0206@users.noreply.github.com>
2019-08-15 23:46:55 +0300

    Add Nato Target

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
 
+var calculateHFOV = document.querySelectorAll(".tabContainer .tabPanel button");;
+
 var lastPanel, lastButton;
 
 function showPanel(currentButton, panelIndex) {
@@ -21,5 +23,12 @@ function showPanel(currentButton, panelIndex) {
 
     navigator.vibrate(100);
 }
+
+function calcHFOV() {
+
+    navigator.vibrate(100);
+
+}
+
 
 showPanel(tabButtons[0], 0);
