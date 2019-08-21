19bad1866e705f487d49d1831a528b73f47d7767
shir0206 <40990488+shir0206@users.noreply.github.com>
2019-08-21 19:08:29 +0300

    Initiate basic functions

--- script.js
+++ script.js
@@ -3,7 +3,34 @@ var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
 
 var calculateHFOV = document.querySelectorAll(".tabContainer .tabPanel button");;
 
-var lastPanel, lastButton;
+var lastPanel;
+var lastButton;
+
+let focalLength;
+let SensorPitch;
+let SensorSizeW;
+let SensorSizeH;
+let HFOV;
+let VFOV;
+let IFOV;
+let natoDet;
+let natoRec;
+let natoIdent;
+let humanDet;
+let humanRec;
+let humanIdent;
+let objectDet;
+let objectRec;
+let LPDet = 2;
+let LPRec = 6;
+let LPIdent = 10;
+let LPDetObject = 1.2;
+let natoTargetW = 2.3;
+let natoTargetH = 2.3;
+let humanTargetW = 0.5;
+let humanTargetH = 1.7;
+let objectTargetW = 0.5;
+let objectTargetH = 0.5;
 
 function showPanel(currentButton, panelIndex) {
     if (lastButton) {
@@ -24,13 +51,24 @@ function showPanel(currentButton, panelIndex) {
     navigator.vibrate(100);
 }
 
-function calcHFOV() {
+function calcHFOVTab() {
 
-    navigator.vibrate(100);
+    focalLength = document.forms["myFcalculateHFOVFormorm"]["valueFocalLength"].value;
+
+    if (focalLength == "") {
+        alert("Name must be filled out");
+        return false;
+    } else {
+        alert("Name is filled out");
+        return true;
+
+    }
+
+    //navigator.vibrate(100);
 
 }
 
-function calcFocalLength() {
+function calcFocalLengthTab() {
 
     navigator.vibrate(100);
 
@@ -43,6 +81,49 @@ function saveSettings() {
 }
 
 
+/** Utils **/
+function calculateIFOV() {
 
+}
+
+function calculateHFOV() {
+
+}
+
+function calculateVFOV() {
+
+}
+
+function calculateNatoDet() {
+
+}
+
+function calculateNatoRec() {
+
+}
+
+function calculateNatoIdent() {
+
+}
+
+function calculateHumanDet() {
+
+}
+
+function calculateHumanRec() {
+
+}
+
+function calculateHumanIdent() {
+
+}
+
+function calculateIdentDet() {
+
+}
+
+function calculateIdentRec() {
+
+}
 
 showPanel(tabButtons[0], 0);
