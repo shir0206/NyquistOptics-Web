363716556c146a829cb9b6b8f8687851a0679be0
shir0206 <40990488+shir0206@users.noreply.github.com>
2019-09-23 16:30:12 +0300

    check coockies

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var lastPanel;
 var lastButton;
 
 
-
+/*
 function showPanel(currentButton, panelIndex) {
 
     if (lastButton) {
@@ -16,8 +16,7 @@ function showPanel(currentButton, panelIndex) {
     }
 
     lastButton = currentButton;
-    lastButton.style.color = "black";
-    lastButton.style.backgroundColor = "#dce6f2";
+    //lastButton.style.backgroundColor = "#dce6f2";
 
     if (lastPanel) {
         lastPanel.style.display = "none";
@@ -26,7 +25,7 @@ function showPanel(currentButton, panelIndex) {
     lastPanel.style.display = "block";
 
     navigator.vibrate(100);
-}
+}*/
 
 function calcHFOVTab() {
 
@@ -45,4 +44,68 @@ function calcHFOVTab() {
 
 }
 
-showPanel(tabButtons[0], 0);
+
+//showPanel(tabButtons[0], 0);
+
+/////////////
+
+
+function setCookie(cname, cvalue, exdays) {
+    /*
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    */
+
+    document.cookie = "target=666";
+}
+
+function getCookie(cname) {
+
+    //Take the cookiename as parameter (cname).
+    //Create a variable (name) with the text to search for (cname + "=")..
+    var name = cname + "=";
+
+    //Decode the cookie string, to handle cookies with special characters, e.g. '$'
+    var decodedCookie = decodeURIComponent(document.cookie);
+
+    //Split document.cookie on semicolons into an array called ca (ca = decodedCookie.split(';')).
+    var ca = decodedCookie.split(';');
+
+    //Loop through the ca array (i = 0; i < ca.length; i++), and read out each value c = ca[i]).
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+
+        //If the cookie is found (c.indexOf(name) == 0), return the value of the cookie (c.substring(name.length, c.length).
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+
+    //If the cookie is not found, return "".
+    return "";
+}
+
+
+function checkCookie() {
+    var username = getCookie("target");
+
+    //If the cookie is set it will display a greeting.
+    if (username != "") {
+        alert("Welcome again " + username);
+    }
+
+    //If the cookie is not set, it will display a prompt box, asking for the name of the user, and stores the username cookie for 365 days, by calling the setCookie function:
+    else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
+    }
+}
+
+checkCookie();
